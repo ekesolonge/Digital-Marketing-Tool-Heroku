7b4d5267e1e5e8d773b08c1dd612ceeccadcbb7b
Ekeso <ekesolonge@gmail.com>
2020-12-30 12:41:49 +0100

    handle db server disconnect

--- models/db.js
+++ models/db.js
@@ -1,17 +1,33 @@
 const mysql = require("mysql"); // mysql module
+var connection;
 
-// Create SQL connection
-const connection = mysql.createConnection({
-  host: "us-cdbr-east-02.cleardb.com",
-  user: "b83cbfeb7950d0",
-  password: "24e38b68",
-  database: "heroku_21d25399649ff1b",
-});
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "b83cbfeb7950d0",
+    password: "24e38b68",
+    database: "heroku_21d25399649ff1b",
+  });
 
-// Test SQL connection
-connection.connect((err, res) => {
-  if (err) throw err;
-  console.log("db server connected");
-});
+  connection.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log("db server connected");
+    }
+  });
+
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
