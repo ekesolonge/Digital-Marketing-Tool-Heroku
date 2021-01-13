a778406e6435980b715de54cad89eb9b6a42fcc4
Ekeso <ekesolonge@gmail.com>
2021-01-13 18:18:11 +0100

    edit Templates route

--- controllers/emailTemplateController.js
+++ controllers/emailTemplateController.js
@@ -74,37 +74,60 @@ const createEmailTemplate = (req, res, next) => {
 // Edit emailTemplates
 const editEmailTemplate = (req, res, next) => {
   connection.query(
-    `SELECT * FROM email_templates WHERE id=${req.params.id}`,
-    (err, db_res) => {
-      if (err) {
-        res.send(err);
-      } else if (db_res.length < 1) {
-        res.status(404).send(`No record found at ID ${req.params.id}`);
-      } else {
-        let email_templates = db_res;
-        let user_id = req.user.data.userId;
-        let name = req.body.name;
-        let json = req.body.json;
-        let html = req.body.html;
-        let id = req.params.id;
-        if (user_id == undefined) {
-          user_id = email_templates[0].user_id;
-        }
-        if (name == undefined) {
-          name = email_templates[0].name;
-        }
-        if (json == undefined) {
-          json = email_templates[0].json;
-        }
-        if (html == undefined) {
-          html = email_templates[0].html;
-        }
-        let sql = `update email_templates set user_id = '${user_id}', name = '${name}', json = '${json}', html = '${html}' where id = ${id}`;
-        connection.query(sql, (err, db_res) => {
-          if (err) return res.status(400).send(err);
-          res.send(`Email Template Updated Successfully at ID: ${id}!`);
+    `select * from email_templates where user_id=${req.user.data.userId}`,
+    (err, resp) => {
+      let emailTemplate = resp.find((x) => x.id == req.params.id);
+      if (emailTemplate == undefined)
+        return res.status(401).send("Cannot edit template you didn't create");
+      let { tempName, json, html } = req.body;
+
+      let jsonContent = JSON.stringify(json);
+
+      let urlImage = "https://api.unlayer.com/v2/export/image";
+      let urlPDF = "https://api.unlayer.com/v2/export/pdf";
+
+      // Convert API key to base64 first
+      const token = Buffer.from(
+        "IQQY9yYHEO4BQZG7cDu00qownOld7LNkhDi7CkPGJrr4zpizjF5xwKUQp6Nj7CBi:"
+      ).toString("base64");
+
+      let options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${token}`,
+        },
+        body: JSON.stringify({
+          displayMode: "email",
+          design: json,
+        }),
+      };
+
+      Promise.all([fetch(urlImage, options), fetch(urlPDF, options)])
+        .then(async ([img, pdf]) => {
+          const image = await img.json();
+          const PDF = await pdf.json();
+          return [image, PDF];
+        })
+        .then((responseText) => {
+          let sql = `update email_templates set name = '${tempName}', json = ?, html = ?, image = ?, pdf = ?, lastEdited = current_timestamp() where id = ${req.params.id}`;
+          connection.query(
+            sql,
+            [
+              jsonContent,
+              html,
+              responseText[0].data.url,
+              responseText[1].data.url,
+            ],
+            (err, db_res) => {
+              if (err) return res.status(400).send("Internal Server Error");
+              res.send(`Email Template Updated Successfully`);
+            }
+          );
+        })
+        .catch((err) => {
+          res.status(400).send("Internal Server Error");
         });
-      }
     }
   );
 };
@@ -132,8 +155,8 @@ const testEmailTemplate = (req, res, next) => {
     `${email}`,
     `${html}`,
     (err3, info) => {
-      if (err3) return res.status(500).send(err3);
-      res.status(201).send("Email Template sent successfully");
+      if (err3) return res.status(500).send("Internal Server Error");
+      res.status(201).send("Email Template Test Mail Sent successfully");
     }
   );
 };
